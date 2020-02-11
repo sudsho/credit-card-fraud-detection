3327b493cdafe65bfa88ce3e2cc489508b5b9f26
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-11 21:35:17 +0530

    added training script with logreg/rf/xgb

---
+++ src/train.py
@@ -0,0 +1,70 @@
+"""Train a fraud detection model.
+
+usage:
+    python -m src.train --config configs/default.yaml
+"""
+import argparse
+import os
+import joblib
+import yaml
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+
+from src.load_data import load
+from src.preprocess import make_split, scale_time_amount
+from src.resample import smote_resample
+
+
+def build_model(cfg):
+    mt = cfg["model"]["model_type"]
+    if mt == "logreg":
+        p = cfg["model"]["logreg"]
+        return LogisticRegression(C=p["C"], max_iter=p["max_iter"], solver="liblinear")
+    if mt == "randomforest":
+        p = cfg["model"]["randomforest"]
+        return RandomForestClassifier(
+            n_estimators=p["n_estimators"], max_depth=p["max_depth"],
+            random_state=cfg["data"]["random_state"], n_jobs=-1)
+    if mt == "xgboost":
+        p = cfg["model"]["xgboost"]
+        return xgb.XGBClassifier(
+            n_estimators=p["n_estimators"], max_depth=p["max_depth"],
+            learning_rate=p["learning_rate"],
+            scale_pos_weight=p.get("scale_pos_weight", 1),
+            objective="binary:logistic", n_jobs=-1)
+    raise ValueError("unknown model_type: %s" % mt)
+
+
+def run(config_path):
+    with open(config_path) as f:
+        cfg = yaml.safe_load(f)
+
+    df = load(cfg["data"]["path"])
+    X_train, X_test, y_train, y_test = make_split(
+        df, test_size=cfg["data"]["test_size"], random_state=cfg["data"]["random_state"])
+
+    X_train, X_test, scaler = scale_time_amount(X_train, X_test, cols=cfg["preprocess"]["scale_columns"])
+
+    if cfg["resample"]["strategy"] == "smote":
+        X_train, y_train = smote_resample(
+            X_train, y_train,
+            sampling_strategy=cfg["resample"]["smote_strategy"],
+            random_state=cfg["resample"]["random_state"])
+
+    model = build_model(cfg)
+    model.fit(X_train, y_train)
+
+    os.makedirs("models", exist_ok=True)
+    joblib.dump(model, "models/model.pkl")
+    joblib.dump(scaler, "models/scaler.pkl")
+    print("saved models/model.pkl")
+    return model
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--config", default="configs/default.yaml")
+    args = ap.parse_args()
+    run(args.config)
