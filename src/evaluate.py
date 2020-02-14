547b1ea4da8359e97066def93163e2419d57be84
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-14 20:11:04 +0530

    wip - PR curve and ROC curve plotting

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,11 +2,18 @@
 
 Class imbalance is severe (~0.17% fraud), so accuracy is useless. We track:
     precision, recall, f1, ROC AUC, average precision (PR-AUC).
+The PR curve is more informative than ROC for this kind of imbalance.
 """
+import os
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
 from sklearn.metrics import (
     classification_report, confusion_matrix,
     roc_auc_score, average_precision_score,
     precision_score, recall_score, f1_score,
+    precision_recall_curve, roc_curve,
 )
 
 
@@ -30,3 +37,36 @@ def report(model, X_test, y_test, threshold=0.5):
     print(classification_report(y_test, pred, digits=4))
     print("ROC AUC:", roc_auc_score(y_test, proba))
     print("PR AUC :", average_precision_score(y_test, proba))
+
+
+def plot_pr_curve(model, X_test, y_test, out_path="reports/pr_curve.png"):
+    proba = model.predict_proba(X_test)[:, 1]
+    p, r, _ = precision_recall_curve(y_test, proba)
+    ap = average_precision_score(y_test, proba)
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    fig, ax = plt.subplots()
+    ax.plot(r, p, label="AP=%.4f" % ap)
+    ax.set_xlabel("Recall")
+    ax.set_ylabel("Precision")
+    ax.set_title("Precision-Recall curve")
+    ax.legend()
+    fig.savefig(out_path, dpi=120, bbox_inches="tight")
+    plt.close(fig)
+    return out_path
+
+
+def plot_roc_curve(model, X_test, y_test, out_path="reports/roc_curve.png"):
+    proba = model.predict_proba(X_test)[:, 1]
+    fpr, tpr, _ = roc_curve(y_test, proba)
+    auc = roc_auc_score(y_test, proba)
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    fig, ax = plt.subplots()
+    ax.plot(fpr, tpr, label="AUC=%.4f" % auc)
+    ax.plot([0, 1], [0, 1], "k--", alpha=0.5)
+    ax.set_xlabel("FPR")
+    ax.set_ylabel("TPR")
+    ax.set_title("ROC curve")
+    ax.legend()
+    fig.savefig(out_path, dpi=120, bbox_inches="tight")
+    plt.close(fig)
+    return out_path
