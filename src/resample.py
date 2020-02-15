41028d8e3c0003d6b31884cae8ad639e8752521e
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-15 22:22:12 +0530

    tweak - random over/under sampler in addition to smote

--- src/resample.py
+++ src/resample.py
@@ -1,12 +1,36 @@
-"""SMOTE resampling for the imbalanced fraud problem.
+"""Resampling for the imbalanced fraud problem.
 
-Only fit SMOTE on the training data, never on test. We oversample the minority
-class (fraud) to a configurable ratio.
+Only fit on training data, never on test. We oversample the minority class
+(fraud) to a configurable ratio. SMOTE is the default; we also support
+random oversampling and random undersampling for comparison.
 """
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTE, RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 
 
 def smote_resample(X_train, y_train, sampling_strategy=0.1, random_state=42):
     sm = SMOTE(sampling_strategy=sampling_strategy, random_state=random_state)
     X_res, y_res = sm.fit_resample(X_train, y_train)
     return X_res, y_res
+
+
+def random_over_resample(X_train, y_train, sampling_strategy=0.1, random_state=42):
+    s = RandomOverSampler(sampling_strategy=sampling_strategy, random_state=random_state)
+    return s.fit_resample(X_train, y_train)
+
+
+def random_under_resample(X_train, y_train, sampling_strategy=0.5, random_state=42):
+    s = RandomUnderSampler(sampling_strategy=sampling_strategy, random_state=random_state)
+    return s.fit_resample(X_train, y_train)
+
+
+def resample(strategy, X_train, y_train, sampling_strategy=0.1, random_state=42):
+    if strategy == "smote":
+        return smote_resample(X_train, y_train, sampling_strategy, random_state)
+    if strategy == "random_over":
+        return random_over_resample(X_train, y_train, sampling_strategy, random_state)
+    if strategy == "random_under":
+        return random_under_resample(X_train, y_train, sampling_strategy, random_state)
+    if strategy == "none":
+        return X_train, y_train
+    raise ValueError("unknown resample strategy: %s" % strategy)
