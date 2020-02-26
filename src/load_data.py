adc76398d0d19b050cd48a4fc95a3992fcacca2a
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-26 10:09:32 +0530

    fix - friendly errors in load() for missing file/cols

--- src/load_data.py
+++ src/load_data.py
@@ -9,7 +9,15 @@ DATA_PATH = os.path.join("data", "raw", "creditcard.csv")
 def load(path=None):
     if path is None:
         path = DATA_PATH
+    if not os.path.exists(path):
+        raise FileNotFoundError(
+            "could not find %s. run scripts/download_data.sh "
+            "(needs the kaggle CLI configured)." % path)
     df = pd.read_csv(path)
+    expected = {"Time", "Amount", "Class"} | {"V%d" % i for i in range(1, 29)}
+    missing = expected - set(df.columns)
+    if missing:
+        raise ValueError("input csv missing columns: %s" % sorted(missing))
     return df
 
 
