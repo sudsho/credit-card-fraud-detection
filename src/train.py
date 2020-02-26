72e271de242a966191aa67ec8265d87823d309b6
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-02-26 20:48:30 +0530

    refactor train - split into prepare/fit_and_eval/save_artifacts

--- src/train.py
+++ src/train.py
@@ -39,30 +39,48 @@ def build_model(cfg):
     raise ValueError("unknown model_type: %s" % mt)
 
 
-def run(config_path):
-    with open(config_path) as f:
-        cfg = yaml.safe_load(f)
+def load_config(path):
+    with open(path) as f:
+        return yaml.safe_load(f)
 
+
+def prepare(cfg):
+    """Load data, split, scale, resample. Returns (X_train, X_test, y_train, y_test, scaler)."""
     df = load(cfg["data"]["path"])
     X_train, X_test, y_train, y_test = make_split(
         df, test_size=cfg["data"]["test_size"], random_state=cfg["data"]["random_state"])
-
-    X_train, X_test, scaler = scale_time_amount(X_train, X_test, cols=cfg["preprocess"]["scale_columns"])
-
+    X_train, X_test, scaler = scale_time_amount(
+        X_train, X_test, cols=cfg["preprocess"]["scale_columns"])
     strat = cfg["resample"]["strategy"]
     if strat != "none":
         X_train, y_train = do_resample(
             strat, X_train, y_train,
             sampling_strategy=cfg["resample"]["smote_strategy"],
             random_state=cfg["resample"]["random_state"])
+    return X_train, X_test, y_train, y_test, scaler
+
+
+def fit_and_eval(model, X_train, X_test, y_train, y_test, threshold=0.5):
+    model.fit(X_train, y_train)
+    metrics = evaluate(model, X_test, y_test, threshold=threshold)
+    return model, metrics
+
+
+def save_artifacts(model, scaler, out_dir="models"):
+    os.makedirs(out_dir, exist_ok=True)
+    joblib.dump(model, os.path.join(out_dir, "model.pkl"))
+    joblib.dump(scaler, os.path.join(out_dir, "scaler.pkl"))
+
+
+def run(config_path):
+    cfg = load_config(config_path)
+    X_train, X_test, y_train, y_test, scaler = prepare(cfg)
     print("after resample:", len(X_train), "rows, fraud frac=",
           float(sum(y_train) / len(y_train)))
 
     model = build_model(cfg)
-    model.fit(X_train, y_train)
-
-    # quick eval on the held-out test set
-    metrics = evaluate(model, X_test, y_test, threshold=cfg["eval"]["threshold"])
+    model, metrics = fit_and_eval(
+        model, X_train, X_test, y_train, y_test, threshold=cfg["eval"]["threshold"])
     print("metrics:", metrics)
 
     proba = model.predict_proba(X_test)[:, 1]
@@ -71,10 +89,7 @@ def run(config_path):
 
     plot_pr_curve(model, X_test, y_test)
     plot_roc_curve(model, X_test, y_test)
-
-    os.makedirs("models", exist_ok=True)
-    joblib.dump(model, "models/model.pkl")
-    joblib.dump(scaler, "models/scaler.pkl")
+    save_artifacts(model, scaler)
     print("saved models/model.pkl")
     return model, metrics
 
